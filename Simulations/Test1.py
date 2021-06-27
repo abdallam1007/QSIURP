f58fc5e12f05e8da8d15f44ce683bb5614d9c130
abdallam1007 <abdallam@andrew.cmu.edu>
2021-06-27 22:31:46 +0300

    ..

--- Simulations/Test1.py
+++ Simulations/Test1.py
@@ -4,7 +4,6 @@ import eplus_env;
 env = gym.make('Eplus-test-v1');
 curSimTime, ob, isTerminal = env.reset(); # Reset the env (creat the EnergyPlus subprocess)
 while not isTerminal:
-    print(ob[8])
     action = [30, 30];
     curSimTime, ob, isTerminal = env.step(action);
 env.end_env(); # Safe termination of the environment after use.
