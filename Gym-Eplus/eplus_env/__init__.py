eff8a764ca160960e1566a9f616b734f03a3df64
abdallam1007 <abdallam@andrew.cmu.edu>
2021-06-28 20:48:44 +0300

    ..

--- Gym-Eplus/eplus_env/__init__.py
+++ Gym-Eplus/eplus_env/__init__.py
@@ -11,7 +11,40 @@ register(
     kwargs={'eplus_path':FD + '/envs/EnergyPlus-8-6-0/', # The EnergyPlus software path
             'weather_path':FD + '/envs/weather/pittsburgh_TMY3.epw', # The epw weather file
             'bcvtb_path':FD + '/envs/bcvtb/', # The BCVTB path
+            'variable_path':FD + '/envs/eplus_models/demo_5z/learning/cfg/1SmallRoom.cfg', # The cfg file
+            'idf_path':FD + '/envs/eplus_models/demo_5z/learning/idf/1SmallRoom.idf', # The idf file
+            'env_name': 'Eplus-test-v1',
+            });
+
+register(
+    id='Eplus-test-v2',
+    entry_point='eplus_env.envs:EplusEnv',
+    kwargs={'eplus_path':FD + '/envs/EnergyPlus-8-6-0/', # The EnergyPlus software path
+            'weather_path':FD + '/envs/weather/pittsburgh_TMY3.epw', # The epw weather file
+            'bcvtb_path':FD + '/envs/bcvtb/', # The BCVTB path
             'variable_path':FD + '/envs/eplus_models/demo_5z/learning/cfg/5ZoneAutoDXVAV_v0.cfg', # The cfg file
             'idf_path':FD + '/envs/eplus_models/demo_5z/learning/idf/5ZoneAutoDXVAV_v0.idf', # The idf file
-            'env_name': 'Eplus-test-v1',
+            'env_name': 'Eplus-test-v2',
+            });
+
+register(
+    id='Eplus-test-v3',
+    entry_point='eplus_env.envs:EplusEnv',
+    kwargs={'eplus_path':FD + '/envs/EnergyPlus-8-6-0/', # The EnergyPlus software path
+            'weather_path':FD + '/envs/weather/pittsburgh_TMY3.epw', # The epw weather file
+            'bcvtb_path':FD + '/envs/bcvtb/', # The BCVTB path
+            'variable_path':FD + '/envs/eplus_models/demo_5z/learning/cfg/1ZoneEvapCooler.cfg', # The cfg file
+            'idf_path':FD + '/envs/eplus_models/demo_5z/learning/idf/1ZoneEvapCooler.idf', # The idf file
+            'env_name': 'Eplus-test-v3',
+            });
+
+register(
+    id='Eplus-test-v4',
+    entry_point='eplus_env.envs:EplusEnv',
+    kwargs={'eplus_path':FD + '/envs/EnergyPlus-8-6-0/', # The EnergyPlus software path
+            'weather_path':FD + '/envs/weather/pittsburgh_TMY3.epw', # The epw weather file
+            'bcvtb_path':FD + '/envs/bcvtb/', # The BCVTB path
+            'variable_path':FD + '/envs/eplus_models/demo_5z/learning/cfg/5ZoneAutoDXVAV_v0.cfg', # The cfg file
+            'idf_path':FD + '/envs/eplus_models/demo_5z/learning/idf/5ZoneAutoDXVAV_v2.idf', # The idf file
+            'env_name': 'Eplus-test-v4',
             });
--- Simulations/Test1.py
+++ Simulations/Test1.py
@@ -1,9 +1,10 @@
 import gym;
 import eplus_env;
 
-env = gym.make('Eplus-test-v1');
+env = gym.make('Eplus-test-v5');
 curSimTime, ob, isTerminal = env.reset(); # Reset the env (creat the EnergyPlus subprocess)
 while not isTerminal:
     action = [30, 30];
+    print(ob[8])
     curSimTime, ob, isTerminal = env.step(action);
 env.end_env(); # Safe termination of the environment after use.
